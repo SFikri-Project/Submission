af5ca6098b61681d260f4395fe626d203abef8c0
S Fikri <136351209+SFikri-Project@users.noreply.github.com>
2024-03-04 03:27:06 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -2,8 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-from babel.numbers import format_currency
-sns.set(style='dark')
 
 ## Membuat fungsi bantuan
 def create_pm25_df(df):
